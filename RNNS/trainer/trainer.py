2a45ad0b135a033b6d546841f0d7759c90154c83
CenIII <cenchuan2013@gmail.com>
2018-11-14 04:47:49 -0500

    lr decay, tf_ratio decay.

--- RNNS/trainer/trainer.py
+++ RNNS/trainer/trainer.py
@@ -14,7 +14,7 @@ class Trainer(object):
 		os.makedirs(self.savePath, exist_ok=True)
 
 	def adjust_learning_rate(self, optimizer, epoch):
-	"""Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+		"""Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
 		lr = self.lr * (0.5 ** (epoch // 30))
 		for param_group in optimizer.param_groups:
 			param_group['lr'] = lr
