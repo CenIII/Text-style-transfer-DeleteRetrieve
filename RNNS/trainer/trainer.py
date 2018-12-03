a36231089b7db295a5360b680a422a0474d87d9b
Siqi Shen <shensq0814@gmail.com>
2018-12-03 16:34:36 -0500

    remove pdb

--- RNNS/trainer/trainer.py
+++ RNNS/trainer/trainer.py
@@ -192,14 +192,11 @@ class LangTrainer(object):
 				inputs = makeInp(next(ld))
 				# import pdb;pdb.set_trace()
 				with torch.set_grad_enabled(True):
-					# import pdb;pdb.set_trace()
 					labels = getLabel(inputs['sentence'])
 					# labels.detach_()
-					# import pdb;pdb.set_trace()
 					outputs = net(inputs['sentence'],inputs['st_inp_lengths'])
-					# import pdb;pdb.set_trace()
+
 					loss = self.celoss(outputs.view(-1,outputs.shape[2]),labels.view(-1))
-					import pdb;pdb.set_trace()
 					loss = loss/torch.sum(inputs['st_inp_lengths']).type(loss.dtype)
 				self.optimizer.zero_grad()
 				loss.backward()
